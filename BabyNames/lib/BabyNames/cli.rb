c1f28e74b9b77060d27d3d1e01403ea5d9ec8641
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-29 00:48:52 +0000

    saving progress

--- BabyNames/lib/BabyNames/cli.rb
+++ BabyNames/lib/BabyNames/cli.rb
@@ -3,32 +3,63 @@ require 'nokogiri'
 require 'pry'
 
 class BabyNames::CLI
-  attr_accessor :male_names, :female_names, :rank_nums, :input
+  attr_accessor :input, :girl_array, :boy_array
 
   def initialize
-    @male_names = {}
-    @female_names = {}
-    @rank_nums = {}
     @input = input
+    @girl_array = []
+    @boy_array = []
   end
 
   def call
-    puts "Would you like the top girl names or top boy names? (Type 'girls' or 'boys' and press enter.)"
+    puts "Would you like the top 10 girl names or top boy names? (Type 'girls' or 'boys' and press enter.)"
     input = gets.chomp
-    errorMsg = "Sorry, you need to choose a gender. Please try again...\n"
-    while input != "girls" || input != "boys"
-      if input == "girls" || input == "Girls"
-        call_table
-      elsif input == "boys" || input == "Boys"
-        call_table
-      else
-        puts errorMsg
-        call
-      end
+    errorMsg = "Sorry, you need to choose a gender. Please try again..."
+
+    if input == "girls" || input == "Girls"
+      girl_names
+      call_girl_table
+    elsif input == "boys" || input == "Boys"
+      boy_names
+      call_boy_table
+    else
+      puts errorMsg
+      call
     end
   end
 
-  def scrape
+  def rank_nums
+    @rank_array = []
+    doc.css("td:nth-child(3)").each(){|name|
+      @rank_array << name.text
+    }
+  end
+
+  def noko_call
+    html = open("https://www.ssa.gov/oact/babynames/")
+    doc = Nokogiri::HTML(html)
+  end
+
+  def girl_names
+    html = open("https://www.ssa.gov/oact/babynames/")
+    doc = Nokogiri::HTML(html)
+
+    doc.css("td:nth-child(3)").each(){|name|
+      @girl_array << name.text if !@girl_array.include?(name.text)
+    }
+  end
+
+  def boy_names
+    html = open("https://www.ssa.gov/oact/babynames/")
+    doc = Nokogiri::HTML(html)
+
+    doc.css("td:nth-child(2)").each(){|name|
+      @boy_array << name.text if !@boy_array.include?(name.text)
+    }
+  end
+
+
+  def scraper
     html = open("https://www.ssa.gov/oact/babynames/")
     doc = Nokogiri::HTML(html)
 
@@ -37,9 +68,65 @@ class BabyNames::CLI
     @girls_head = doc.css(".c-table th:nth-child(3)").text.upcase + "S"
   end
 
-  def call_table
-    scrape
-    puts "<<{}--[#{@rank_head}]---[#{@boys_head}]---[#{@girls_head}]--{}>>"
-    binding.pry
+  def call_girl_table
+    scraper
+    count = 1
+    puts "<<{}--[#{@rank_head}]-[#{@girls_head}]--{}>>"
+    @girl_array.each(){|name|
+      puts "   >>  [#{count}]:" + "     #{name}" unless count == 10
+      count += 1
+    }
+    puts "   >>  [#{count-1}]:" + "    #{@girl_array[9]}"
+    puts "<<{}---------END-----------{}>>"
+
+    boys_this_time?
+  end
+
+  def call_boy_table
+    scraper
+    count = 1
+    puts "<<{}--[#{@rank_head}]-[#{@boys_head}]--{}>>"
+    @boy_array.each(){|name|
+      puts "   >>  [#{count}]:" + "     #{name}" unless count == 10
+      count += 1
+    }
+    puts "   >>  [#{count-1}]:" + "    #{@boy_array[9]}"
+    puts "<<{}---------END-----------{}>>"
+
+    girls_this_time?
+  end
+
+  def girls_this_time?
+  puts " "
+  puts "//////////////////////////////////"
+  puts "Would you like to know girl names?"
+  puts "(type 'yes' or 'no')"
+  puts "//////////////////////////////////"
+  answer = gets.chomp
+
+    if answer == "yes" || answer == "Yes"
+      puts " "
+      girl_names
+      call_girl_table
+    elsif answer == "no" || answer == "No"
+      puts "Goodbye! :)"
+    end
+  end
+
+  def boys_this_time?
+  puts " "
+  puts "/////////////////////////////////"
+  puts "Would you like to know boy names?"
+  puts "(type 'yes' or 'no')"
+  puts "/////////////////////////////////"
+  answer = gets.chomp
+
+    if answer == "yes" || answer == "Yes"
+      puts " "
+      boy_names
+      call_boy_table
+    elsif answer == "no" || answer == "No"
+      puts "Goodbye! :)"
+    end
   end
 end
